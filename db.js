1f18115eb84e0d51af33501eaf8a52af5a659ff8
ramiclaguitan <ramlag1819@gmail.com>
2024-02-15 12:25:44 -0800

    Install dotenv

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "cardlist",
-  password: "Akosinonoy1819",
-  port: 5432
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 module.exports = pool;
